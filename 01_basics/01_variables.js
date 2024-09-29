58fa38ee6c5124a341ac91b4fa52e07584b2c1c3
rohit-thakurr <86067716+rohit-thakurr@users.noreply.github.com>
2024-09-29 16:36:27 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14223
+let accountEmail = "rohit@google.com"
+var accountPassword = "123456"
+
+accountCity = "jaipur"
+let accountState;
+
+
+//accountId = 2223
+/*
+    Use let instead of var, because of scpping
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" // To treat all codes in this file as newer version of JS (now it's by default)
+
+// alert(3+3). // We are using it in nodejs & it can be use in developer console in broweser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;   // undefined
+let newState = null // value is not defined that is it have empty value
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined
+// symbol =>  use for uniqueness
+
+// object
+
+console.log(typeof age)
+console.log( typeof null)   // type is object
+console.log( typeof undefined);  // type is undefined
