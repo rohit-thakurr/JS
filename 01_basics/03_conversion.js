c2c8649b2dd0d27fc6fbe176be9551abd7578d7f
rohit-thakurr <86067716+rohit-thakurr@users.noreply.github.com>
2024-09-30 08:28:36 +0000

    converisons

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,42 @@
+
+let age = "33"
+
+console.log( typeof age)
+console.log( typeof(age))    //Both can be use
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber)
+
+let newAge = "33abc"
+let wrongAge = Number(newAge)
+
+console.log(typeof wrongAge)    // Type here is also a number
+console.log(wrongAge)   // value assign in this NAN which is not a number value have a datatype of number
+
+let scoreOne = Number(null);       // Convert this value to 0 as value has not been assigned
+let scoreTwo = Number(undefined);   // Convert this value to NAN
+
+console.table([scoreOne,scoreTwo])
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.table([stringNumber, typeof stringNumber])
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = 1 + "2"
+let str2 = "1" + 2
+console.table([str1, typeof str1, str2, typeof str2])
+
+console.log("2" + 1 + 3)
+console.log( 2 + 1 + "3")
