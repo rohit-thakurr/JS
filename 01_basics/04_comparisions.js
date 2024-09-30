97c1584e1179cf3da1546879de4e29b367c41ae6
rohit-thakurr <86067716+rohit-thakurr@users.noreply.github.com>
2024-09-30 10:02:19 +0000

    comparison

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);              true ( converts "2" into number for comparision)
+// console.log("02" > 1);             true
+
+console.log(null > 0);             //false ( converts null to number 0, 0>0 -> false)
+console.log(null == 0);            //false
+
+/*The equality (==) operator does not perform type conversion in the same way here.
+ In this case, null is only loosely equal to undefined, but not to 0. So, null == 0 is false because null and 0 are of different types and are not considered equal under loose equality.
+*/
+
+console.log(null >= 0);           // true ( converts null to number 0, 0>=0 -> true)
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
