489c5ea1b85ba2f60defb2dafd1e5a5584d5038d
rohit-thakurr <86067716+rohit-thakurr@users.noreply.github.com>
2024-10-07 07:32:33 +0000

    maths in js

---
+++ 01_basics/07_maths.js
@@ -0,0 +1,17 @@
+
+console.log(Math)     // Object type have many properties and methods
+
+console.log(Math.PI)        // Property of Math to get PI value
+console.log(Math.SQRT2)     // Property to get sqrt of 2
+
+
+console.log( Math.abs(-4))   // Absoulte method in Math object
+console.log(Math.round(4.3)) // Round off method
+console.log(Math.round(4.6))
+
+console.log(Math.ceil(4.3))    // Ceil means nearest greater integer that is top value
+console.log(Math.floor(4.3))   // Floor means nearest smallest integer that is bottom value
+
+console.log(Math.random())     // Gives a random value between 0 to 1
+
+console.log( Math.floor(Math.random()*6 + 1) )  // Give random value between 1 to 6
