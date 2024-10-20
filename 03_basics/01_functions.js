5a7c8bae14d2139bc56a34ae850a6b42001e339c
rohit-thakurr <86067716+rohit-thakurr@users.noreply.github.com>
2024-10-20 08:15:31 +0000

    basics of functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,3 +20,27 @@ addTwoNumbers();               // Passing no arguments returns NAN
 
 addTwoNumbers(2);               // Passing only one argument results in 2 + undefined => NAN
 addTwoNumbers(2,3);
+
+const result = addTwoNumbers(2,3);
+console.log(result)            // Since the function does not have a return statement, it implicitly returns undefined.
+
+
+function loggedIn(userName){
+    return `${userName} just logged in`;
+}
+
+console.log( loggedIn("Rohit") );     // Rohit just logged in
+console.log( loggedIn() );            // undefined just logged in
+/*
+    In JavaScript, when you declare a variable but don't assign any value to it,
+    it is automatically initialized with the value undefined. 
+    
+    This applies to function parameters as well, meaning if you don't pass any argument when calling the function,
+    the corresponding parameter will be undefined by default.
+
+    When you declare a variable like let num1; without assigning any value to it, the following happens:
+
+    let num1;  so basically if i declare a variable like this ->
+    Type: The type of num1 is undefined. In JavaScript, undefined is a type on its own.
+    Value: The value of num1 is also undefined.
+*/
