6d6da51462f2fcfbd7ebc7330133dcae62d9366c
rohit-thakurr <86067716+rohit-thakurr@users.noreply.github.com>
2024-11-29 11:30:28 +0000

    implicit and explicit functions

--- 03_basics/08_arrowFunctions.js
+++ 03_basics/08_arrowFunctions.js
@@ -19,4 +19,38 @@ const explicitFunc = (num1,num2) => {
 
     Can convert any one liner explicit function to implicit
 */
-const implicitFunc = (num1, num2) => num1 * num2;
+const implicitFunc = (num1, num2) => num1 * num2;  
+console.log( implicitFunc(1,2) )          
+
+const implicitFunc2 = (num1 , num2) => { name : "Rohit"};    // Will return undefined
+console.log(implicitFunc2(1,2));
+
+const implicitFunc3 = (num1,num2) => ({ name : "Thakur"})     // Now it will return value as object wrraped in ()
+console.log( implicitFunc3(1,2))
+
+/*
+    Arrow functions can have an implicit return if you omit the curly braces ({})
+    around the function body. In such cases, the function automatically returns the result of the expression
+    that follows the arrow (=>).
+    like in this case -> const implicitFunc = (num1, num2) => num1 * num2;  It returned value
+
+
+    Why Your implicitFunc2 Returns undefined ?
+    When you use curly braces ({}) in an arrow function, JavaScript interprets this as the start of a block,
+    not an object literal. Within a block, 
+    if you don't explicitly use the return keyword, the function returns undefined by default.
+
+    It takes implicitFunc2 function like this
+    const implicitFunc2 = (num1, num2) => {
+            name: "Rohit"; // This is interpreted as a labeled statement, not an object
+    };
+
+    & here it is actually an explicit function with no return value so it return undefined.
+
+
+
+    Why Your implicitFunc3 Works ?
+    When you wrap the object literal in parentheses (), 
+    it forces JavaScript to interpret the content as an expression, not a block.
+    This is because parentheses have higher precedence and create an explicit grouping.
+*/
